01b6155e56efdb7412b4558352ebec5e45ac393f
smiroshn <smiroshn@akamai.com>
2017-05-25 23:06:36 +0300

    Solved a buffer issue on MAC

--- src/engineTester/Boot.java
+++ src/engineTester/Boot.java
@@ -6,6 +6,7 @@ import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import renderEngine.DisplayManager;
 
 /**
  * Created by smiroshn on 5/24/17.
@@ -14,7 +15,7 @@ public class Boot {
     public Boot() {
         Display.setTitle("Understanding OpenGL...");
         try {
-            Display.setDisplayMode(new DisplayMode(320, 200));
+            Display.setDisplayMode(new DisplayMode(640, 480));
             Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
@@ -23,26 +24,21 @@ public class Boot {
         GL11.glMatrixMode(GL11.GL_PROJECTION); //?
         GL11.glLoadIdentity();//?
         GL11.glOrtho(0, 600, 400, 0, 1, -1); //Camera
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
         while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
 
-            // Useful for mouse degug
-            float mouseX = Mouse.getX();
-            float mouseY = Mouse.getY();
-            System.out.println("Mouse :x " + mouseX + "Mouse :y " + mouseY);
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT); //very important to clear the buffer
 
-
-            GL11.glBegin(GL11.GL_POLYGON);
-            GL11.glVertex3f(120.0f, 120.0f, 0.0f);
-            GL11.glVertex3f(180.0f, 120.0f, 0.0f);
-            GL11.glVertex3f(180.0f, 180.0f, 0.0f);
-            GL11.glVertex3f(120.0f, 180.0f, 0.0f);
-
-
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex2f(10, 10);
+            GL11.glVertex2f(100, 100);
             GL11.glEnd();
 
+
             Display.update();
-            Display.sync(60);
+            Display.sync(30);
+
 
         }
 
